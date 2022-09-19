4357c341b6fd7647b60ba87dfc4bc915fe1eeb8f
jlonuz <lonuz.j@gmail.com>
2022-09-19 00:30:36 -0300

    create completed and deleted method and on change value method in search

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,15 +2,28 @@ import React from 'react';
 import './TodoItem.css';
 
 function TodoItem(props) {
+  const onCompleted = () => {
+    alert('Ya completaste el todo ' + props.text);
+  }
+
+  const onDeleted = () => {
+    alert('Borraste el todo ' + props.text);
+  }
   return (
     <li className="TodoItem">
-      <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>
+      <span
+        className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
+        onClick={onCompleted}
+      >
         √
       </span>
       <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
         {props.text}
       </p>
-      <span className="Icon Icon-delete">
+      <span
+        className="Icon Icon-delete"
+        onClick={onDeleted}
+      >
         X
       </span>
     </li>
