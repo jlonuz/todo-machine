5915b5f60e12f479537e1e47f677ac2f3a8fd938
jlonuz <lonuz.j@gmail.com>
2022-09-19 11:02:58 -0300

    adding states to app component

--- src/App.js
+++ src/App.js
@@ -6,21 +6,44 @@ import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
 //import './App.css';
 
-const todos = [
-  { text: 'cortar cebolla', completed: true },
+const defaultTodos = [
+  { text: 'cortar cebolla', completed: false },
   { text: 'terminar curso intro a react', completed: false },
   { text: 'llorar con la llorona', completed: false }
 ];
 
 
 function App() {
+  const [ todos, setTodos] = React.useState(defaultTodos);
+  const [ searchValue, setSearchValue] = React.useState('');
+
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const totalTodos = todos.length;
+
+  let searchedTodos = [];
+  if(!searchValue.length >= 1){
+    searchedTodos = todos;
+  } else {
+    searchedTodos = todos.filter(todo => {
+    const todoText = todo.text.toLowerCase();
+    const searchText = searchValue.toLowerCase();
+    return todoText.includes(searchText);
+    })
+  }
+
   return (
     <>
-      <TodoCounter />
+      <TodoCounter
+        total={totalTodos}
+        completed={completedTodos}
+      />
 
-      <TodoSearch />
+      <TodoSearch
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
       <TodoList>
-        { todos.map( todo => (
+        { searchedTodos.map( todo => (
           <TodoItem
             key={todo.text}
             text={todo.text}
